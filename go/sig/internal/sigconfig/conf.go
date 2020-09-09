42bbaeced4e3b7231542891bbbb9edee7f337b3a
Matthias Frei <matthias.frei@inf.ethz.ch>
2020-09-09 11:27:45 +0200

    sig: simple config (#75)
    * sig: remove default prometheus address (#3850)
    
    The SIG was setting a default prometheus address, contrary to what is
    documented; the docs explicitly say that if the address is unset, the
    metrics will not be exported. This default value would also make it
    impossible to disable exporting metrics.
    
    Remove the undocumented, unneeded default value.
    
    * sig: remove IA from config, make IP optional (#3849)
    
    The local IA in the sig.toml config is redundant as it can be obtained
    from sciond. Simply removed the option.
    The bind IP option is made optional by defaulting to a sensible local
    IP, using the same logic used e.g. in showpaths.
    
    Both of these changes combined allow us to package a default sig.toml
    config file that (in most cases) "just works" without modifications.
    
    * sig: keep isd_as option for backwards compatibility

--- go/sig/internal/sigconfig/conf.go
+++ go/sig/internal/sigconfig/conf.go
@@ -83,9 +83,9 @@ type SigConf struct {
 	ID string `toml:"id,omitempty"`
 	// The SIG config json file. (required)
 	SIGConfig string `toml:"sig_config,omitempty"`
-	// IA the local IA (required)
-	IA addr.IA `toml:"isd_as,omitempty"`
-	// IP the bind IP address (required)
+	// _IA is *ignored*, kept only for backwards compatibility
+	_IA addr.IA `toml:"isd_as,omitempty"`
+	// IP the bind IP address (optional, default determined based on route to control service)
 	IP net.IP `toml:"ip,omitempty"`
 	// Control data port, e.g. keepalives. (default DefaultCtrlPort)
 	CtrlPort uint16 `toml:"ctrl_port,omitempty"`
@@ -116,15 +116,6 @@ func (cfg *SigConf) Validate() error {
 	}
 	if cfg.SIGConfig == "" {
 		return serrors.New("sig_config must be set!")
-	}
-	if cfg.IA.IsZero() {
-		return serrors.New("isd_as must be set")
-	}
-	if cfg.IA.IsWildcard() {
-		return serrors.New("Wildcard isd_as not allowed")
-	}
-	if cfg.IP.IsUnspecified() {
-		return serrors.New("ip must be set")
 	}
 	if cfg.CtrlPort == 0 {
 		cfg.CtrlPort = DefaultCtrlPort
